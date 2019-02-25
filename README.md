066447779c101a50e4719cc7d3d25222de0830a8
Paul Patterson <pddpatterson@gmail.com>
2019-02-25 12:12:10 +0200

    updated readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stock Market Ticker
+
+An early concept for a game challenging a user to try beat the market for a random stock at a random time-period.
+
+The scene is that every 500ms a new data point is added to the graph, and the user would be allowed to buy and sell 3 times throughout the round, naturally not knowing where this stock is going as the data points are added.
+
+![](example.gif)
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.listen(port, () => console.log(`Express listening and serving on port: ${port}.`));
+app.listen(port, () => console.log(`Express listening and serving on port: ${port}.`))
