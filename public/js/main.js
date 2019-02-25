1f35a827b6a11948baff9886c73f7b91425c9ecb
Paul Patterson <pddpatterson@gmail.com>
2019-02-25 10:12:44 +0200

    .

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,58 @@
-import * as API from './api';
-import * as CONFIG from './config';
-import Chart from './chart';
+import * as API from './api'
+import * as CONFIG from './config'
 
-const stockChart = new Chart();
+import Chart from './chart'
+
+const stockChart = new Chart()
 
 // import sampleData from '../sampleData.json';
 
 const sliceRandomDataSelection = (dataPoints, selectionLength) => {
-  const startIndex = Math.floor(Math.random() * (dataPoints.length - selectionLength));
-  return dataPoints.slice(startIndex, startIndex + selectionLength);
-};
+  const startIndex = Math.floor(Math.random() * (dataPoints.length - selectionLength))
+  return dataPoints.slice(startIndex, startIndex + selectionLength)
+}
 
 const parseDateBasic = (date, prefix = '') => {
-  const [day, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()];
+  const [day, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()]
 
   return {
     [`${prefix}Date`]: date,
     [`${prefix}Day`]: day,
     [`${prefix}Month`]: month,
     [`${prefix}Year`]: year
-  };
-};
+  }
+}
 
 const getPeriodMetadata = data => {
-  const startDate = data[0].date;
-  const endDate = data[data.length - 1].date;
+  const startDate = data[0].date
+  const endDate = data[data.length - 1].date
 
   const periodDateMeta = Object.assign(
     {},
     parseDateBasic(startDate, 'start'),
     parseDateBasic(endDate, 'end')
-  );
+  )
 
-  return Object.assign({}, periodDateMeta);
-};
+  return Object.assign({}, periodDateMeta)
+}
 
 const getDataExerpt = data => {
-  const dataExcerpt = sliceRandomDataSelection(data.dataPoints, 120);
-  const excerptMetadata = getPeriodMetadata(dataExcerpt);
-  const symbol = data.metadata['2. Symbol'];
-  const metadata = Object.assign({}, excerptMetadata, { symbol });
+  const dataExcerpt = sliceRandomDataSelection(data.dataPoints, 120)
+  const excerptMetadata = getPeriodMetadata(dataExcerpt)
+  const symbol = data.metadata['2. Symbol']
+  const metadata = Object.assign({}, excerptMetadata, { symbol })
 
-  return { data: dataExcerpt, metadata };
-};
+  return { data: dataExcerpt, metadata }
+}
 
 window.onload = async () => {
-  const tradingPair = CONFIG.getRandomTradingPair();
+  const tradingPair = CONFIG.getRandomTradingPair()
 
-  const data = await API.fetchData(tradingPair);
+  const data = await API.fetchData(tradingPair)
 
   // const formattedData = API.formatRawData(sampleData);
-  const formattedData = API.formatRawData(data);
-  const dataExerpt = getDataExerpt(formattedData);
+  const formattedData = API.formatRawData(data)
+  const dataExerpt = getDataExerpt(formattedData)
 
-  stockChart.renderGraph(dataExerpt);
-};
+  stockChart.renderGraph(dataExerpt)
+}
